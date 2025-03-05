e0fcb5d48ebce7f2eab063aa47d51dcf68d452c6
Ryan Park <ryan81.park@gmail.com>
2025-03-05 17:10:08 +1100

    ui comments

--- components/comments/Comments.tsx
+++ components/comments/Comments.tsx
@@ -7,56 +7,44 @@ import { SubmitButton } from "@/components/submit-button";
 import { postComments } from "@/app/actions";
 
 interface CommentProps {
-  id: number;
-  comments: CommentType[];
-  user: any; //to do
+	id: number;
+	comments: CommentType[];
+	user: any; //to do
 }
 
 interface CommentType {
-  comment: string;
-  nickname: string;
+	comment: string;
+	nickname: string;
 }
 
 export function Comments({ id, comments, user }: CommentProps) {
-  const [isEdit, setIsEdit] = useState(false);
-  const [isShowComments, setShowComments] = useState(false);
+	const [isShowComments, setShowComments] = useState(false);
 
-  return (
-    <>
-      <div
-        className="flex cursor-pointer"
-        onClick={() => setShowComments(!isShowComments)}
-      >
-        <MessageSquareMore className="mr-1 cursor-pointer" /> {comments.length}
-      </div>
-      {isShowComments &&
-        comments?.map((comment: CommentType) => {
-          return (
-            <div>
-              {comment.nickname} , {comment.comment}
-            </div>
-          );
-        })}
-      {user && (
-        <>
-          {!isEdit ? (
-            <MessageSquareMore
-              onClick={() => setIsEdit(true)}
-              className="cursor-pointer"
-            />
-          ) : (
-            <form>
-              <Input type="textarea" name="comments" />
-              <input type="hidden" name="dreamId" value={id} />
+	return (
+		<>
+			<div
+				className="flex cursor-pointer"
+				onClick={() => setShowComments(!isShowComments)}
+			>
+				<MessageSquareMore className="mr-1 cursor-pointer" /> {comments.length}
+			</div>
+			{isShowComments &&
+				comments?.map((comment: CommentType) => {
+					return (
+						<div>
+							{comment.nickname} , {comment.comment}
+						</div>
+					);
+				})}
+			{user && isShowComments && (
+				<form>
+					<Input type="textarea" name="comments" />
+					<input type="hidden" name="dreamId" value={id} />
 
-              <SubmitButton formAction={postComments}>
-                Make comment
-              </SubmitButton>
-              <button onClick={() => setIsEdit(false)}>cancel</button>
-            </form>
-          )}
-        </>
-      )}
-    </>
-  );
+					<SubmitButton formAction={postComments}>Make comment</SubmitButton>
+					<button onClick={() => setShowComments(false)}>cancel</button>
+				</form>
+			)}
+		</>
+	);
 }
