4dc5fd5ca849572ac50299968ae68fde556025d6
RyanP <ryan81.park@gmail.com>
2025-04-26 11:01:48 +1000

    Login page

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -9,63 +9,103 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
 
   return (
-    <>
-      <form className="flex-1 flex flex-col min-w-64">
-        <h1 className="text-2xl font-medium bg-yellow">Sign in</h1>
-        <p className="text-sm text-foreground">
-          Don't have an account?{" "}
-          <Link
-            className="text-foreground font-medium underline"
-            href="/sign-up"
-          >
-            Sign up
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <div className="flex justify-between items-center">
-            <Label htmlFor="password">Password</Label>
-            <Link
-              className="text-xs text-foreground underline"
-              href="/forgot-password"
-            >
-              Forgot Password?
-            </Link>
+    <div className="mt-10">
+      <h1 className="text-2xl font-satoshi mb-10 pl-10 pr-10">
+        Explore, understand, and connect over dreams
+      </h1>
+      <div className="p-10 rounded-md bg-darkPurple shadow-md relative">
+        <form className="flex-1 flex flex-col min-w-64">
+          <div className="absolute right-[-107px] top-[-200px]">
+            <Image
+              src="/bitch.png"
+              width={175}
+              height={349}
+              alt="here is my bitch"
+            />
           </div>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            required
-          />
+          <h1 className="text-2xl text-center">Sign in</h1>
+          <p className="text-sm text-foreground text-center mb-5">
+            Don't have an account?{" "}
+            <Link
+              className="text-foreground font-medium underline"
+              href="/sign-up"
+            >
+              Sign up
+            </Link>
+          </p>
+          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              className="bg-purple border-purple placeholder-yellow text-white focus-visible:ring-0 focus-visible:ring-offset-0"
+              placeholder="you@example.com"
+              name="email"
+              required
+            />
+            <div className="flex justify-between items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                className="text-xs text-foreground underline"
+                href="/forgot-password"
+              >
+                Forgot Password?
+              </Link>
+            </div>
+            <Input
+              className="bg-purple border-purple placeholder-yellow text-white focus-visible:ring-0 focus-visible:ring-offset-0"
+              placeholder="Your password"
+              type="password"
+              name="password"
+              required
+            />
 
-          <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-            Sign in
-          </SubmitButton>
-          {/* Facebook Sign-Up Button */}
-          <Button
-            onClick={signUpFacebookAction}
-            className="mt-4"
-            variant="outline"
-          >
-            Sign up with Facebook
-          </Button>
-          <Button
-            onClick={signUpGoogleAction}
-            className="mt-4"
-            variant="outline"
-          >
-            Sign up with Google
-          </Button>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-    </>
+            <SubmitButton
+              pendingText="Signing In..."
+              formAction={signInAction}
+              className="bg-yellow text-purple font-bold md:w-auto inline-flex mt-4"
+            >
+              Sign in
+            </SubmitButton>
+            <div className="pt-10 pb-10 text-center">- Or Sign In with -</div>
+
+            {/* Facebook Sign-Up Button */}
+            <div className="flex justify-evenly">
+              <Button
+                onClick={signUpFacebookAction}
+                className="bg-yellow text-purple font-bold md:w-auto inline-flex"
+                variant="outline"
+              >
+                <Image
+                  src="/facebook.svg"
+                  width={30}
+                  height={30}
+                  alt="Picture of the author"
+                />
+                Sign up with Facebook
+              </Button>
+              <Button
+                onClick={signUpGoogleAction}
+                className="bg-yellow text-purple font-bold md:w-auto inline-flex"
+                variant="outline"
+              >
+                <Image
+                  src="/google.svg"
+                  width={30}
+                  height={30}
+                  alt="Picture of the author"
+                />
+                Sign up with Google
+              </Button>
+            </div>
+            <FormMessage message={searchParams} />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
